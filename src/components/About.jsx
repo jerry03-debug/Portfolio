acc482782005c71e4045edf2c50aad665439794f
Diery <dierydia@esp.sn>
2023-08-16 00:36:31 +0000

    comebacl

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
-// import Tilt from 'react-tilt'
+import Tilt from 'react-parallax-tilt'
 import { motion } from "framer-motion";
 import {styles} from '../style'
 import { services } from "../constants";
 import { fadeIn,textVariant } from "../utils/motion";
+
 const ServiceCard = ({title,icon,index})=>{
   return(    
-    <p>{title}</p>
+      <Tilt className="xs:w-[300px] w-full " >
+        <motion.div variants={fadeIn("right","spring",0.5*index)} className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card" >
+          <div
+            options={{
+              max:45,
+              scale:1.05,
+              speed:450,
+            }}
+            className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[250px] flex justify-evenly items-center flex-col ">
+            <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+            <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+
+
+          
+          
+
+          </div>
+
+         </motion.div> 
+      </Tilt>
   )
 }
     
+
+
 const About = () => {
   return (
     <>
